c037ecb425652e08f134d1946cdbaa2cd25fdfe2
MoonVN <60611504+MoonVN571@users.noreply.github.com>
2023-06-22 21:14:52 +0700

    fix

--- src/events/Bot/messageDelete.ts
+++ src/events/Bot/messageDelete.ts
@@ -3,6 +3,8 @@ import { serverModel } from "../../databases/server-model";
 import { Bot } from "../../struct/Bot";
 
 export async function execute(client: Bot, message: Message) {
+	if (message.channel.id !== client.config.whitelist.channelId) return;
+
 	const db = await serverModel.findOne({ guildId: message.guildId });
 	if (!db) return;
 
